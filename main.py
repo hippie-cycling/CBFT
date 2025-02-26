e85208b71a6734f4f474b7282448e57cde35904b
DN <46326039+hippie-cycling@users.noreply.github.com>
2025-02-26 15:38:26 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -7,13 +7,13 @@ GREY = '\033[38;5;238m'
 RESET = '\033[0m'
 
 def display_menu():
-    print("\n" + "="*40)
-    print("Please select a cipher to run:")
+    print(f"\n" + f"{GREY}={RESET}"*40)
+    print("Select a cipher to run:")
     print(f"{YELLOW}0.{RESET} Help")
     print(f"{YELLOW}1.{RESET} Gromark Cipher")
     print(f"{YELLOW}2.{RESET} Gronsfeld Cipher")
     print(f"{YELLOW}3.{RESET} Exit")
-    print("="*40)
+    print(f"{GREY}={RESET}"*40)
 
 def main():
     # Display the logo once at the start
@@ -29,10 +29,10 @@ def main():
 ░ ░       ░                       
 ░              ░                  
 {RESET}""")
-    print("       Cipher Brute Force Tool")
-    print("="*40)
+    print(f"\033[48;5;237mCipher Brute Force Tool{RESET}")
+    print(f"{GREY}={RESET}"*40)
     print(f"Developed by Daniel Navarro\n{YELLOW}(https://github.com/hippie-cycling){RESET}")
-    print("="*40)
+    print(f"{GREY}={RESET}"*40)
     
     while True:
         display_menu()
@@ -40,10 +40,10 @@ def main():
         choice = input(f"Enter your choice ({YELLOW}0-3{RESET}): ").strip()
         
         if choice == '0':
-            print("""\n
-- Gronsfield: The user can input a custom alphabet and plaintext words to be found. The brute force will check every key and output the keys that decript the plaintext words and the keys that generate an IoC close to English (0.06 <= ioc <= 0.07) for further analysis.
-- Gromark: Input the ciphertext, the key for the keyed alphabet and the size of the primer key and it will brute force until one of the input plaintext is found and or the IoC of the plaintext closely matched english (in case the cipher is layered with transposition). Don't go crazy with the key size as it might literally tak forever to compute.
-- Vigenere WIP: The user can input a custom alphabet and plaintext words to be found. The brute force will check every word in a large list of English words and output the keys that decript the plaintext words and/or the keys that generate an IoC close to English (0.06 <= ioc <= 0.07) for further analysis.
+            print(f"""\n
+- {YELLOW}Gronsfield:{RESET} The user can input a custom alphabet and plaintext words to be found. The brute force will check every key and output the keys that decript the plaintext words and the keys that generate an IoC close to English (0.06 <= ioc <= 0.07) for further analysis.
+- {YELLOW}Gromark:{RESET} Input the ciphertext and it will brute force all words from words_alpha.text. The script will output the word and key if one of the input plaintext is found and or the input words can be found in the output (even if transposed).
+- {YELLOW}Vigenere WIP:{RESET} The user can input a custom alphabet and plaintext words to be found. The brute force will check every word in a large list of English words and output the keys that decript the plaintext words and/or the keys that generate an IoC close to English (0.06 <= ioc <= 0.07) for further analysis.
 \nDon't go crazy with the key size as it might literally take forever to compute. Recommended key size is 1-9 digits, note the primer/s and extrapolate.""")
         elif choice == '1':
             print("\nYou selected: Gromark Cipher")
